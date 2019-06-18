6590f053d97e1ad10ae983961ffd7971703aaacf
arnoutc <arnout@catorarn.com>
2019-06-18 22:09:26 +0100

    created test for priority in new todo item

--- cypress/integration/todo-app/todoapp_spec.js
+++ cypress/integration/todo-app/todoapp_spec.js
@@ -2,9 +2,16 @@
 
 describe('my first test', function(){
   it('visit the todo-app', function(){
-    
+
     cy.visit('http://localhost:8000');
 
-    expect(true).to.equal(true);
+    let el = cy.get('#new-todo');
+
+    el.should('have.text','');
+
+    el.should('have.attr','placeholder','What needs to be done?');
+
+    el.should('have.class', 'priority');
+
   });
 });
