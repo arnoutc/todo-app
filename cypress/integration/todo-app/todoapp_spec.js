1c5303ab20b0ff83740397700addd08f5aa39b15
arnoutc <arnout@catorarn.com>
2019-07-01 18:35:22 +0100

    added custom cypress command to the mix to automatically create multiple todos

--- cypress/integration/todo-app/todoapp_spec.js
+++ cypress/integration/todo-app/todoapp_spec.js
@@ -1,39 +1,51 @@
 /// <reference types="Cypress" />
 
 describe('my first test', function(){
+
+  let TODO_ITEM_ONE = 'buy some cheese';
+  let TODO_ITEM_TWO = 'feed the cat';
+  let TODO_ITEM_THREE = 'book a doctors appointment';
+
+  beforeEach(function(){
+    // By default Cypress will automatically
+      // clear the Local Storage prior to each
+      // test which ensures no todos carry over
+      // between tests.
+      //
+      // Go out and visit our local web server
+      // before each test, which serves us the
+      // TodoMVC App we want to test against
+      //
+      // We've set our baseUrl to be http://localhost:8000
+      // which is automatically prepended to cy.visit
+      //
+      // https://on.cypress.io/api/visit
+    cy.visit('/');
+  });
+
   it('visit the todo-app', function(){
+    cy.createDefaultTodos();
+    // this is an example of another custom command
+    // since we repeat the todo creation over and over
+    // again. It's up to you to decide when to abstract
+    // repetitive behavior and roll that up into a custom
+    // command vs explicitly writing the code.
 
-    cy.visit('http://localhost:8000');
+    //another way of creating some todos with a custom command:
+    // cy.createTodo(`    ${TODO_ITEM_TWO}    `);
+    // cy.createTodo(`    ${TODO_ITEM_THREE}    `);
 
-    //set text
-    let el = cy.get('[data-cy=new-todo]');
-    el.should('have.attr','placeholder','What needs to be done?');  
+    //when setting a text
+    cy.get('[data-cy=new-todo]')
+    .type(TODO_ITEM_ONE);
 
-    let text = 'supercalifragilisticexpialidocious';
-    el.type(text);
+    cy.get('[data-cy=priority-btn]')
+      .trigger('click');
 
-    let prio = cy.get('[data-cy=priority-btn]');
-    prio.trigger('click');
-
-    cy.get('[data-cy=new-todo]').should('have.value',text);
-
-  
-    //then
-    cy.get('[data-cy=todo-list]').children().should('have.class', 'priority');
-
-
-    //when second item is added with priority
-
-    //there are 2 li's with priority class
-
-    //when third item is added without priority
-
-    //when click Priority filter
-
-    //then 2 items should show in the list
-
-
-
+    //and then the list should have a prioritized item
+    cy.get('[data-cy=todo-list]')
+    .children()
+    .should('have.class', 'priority');
 
   });
 });
