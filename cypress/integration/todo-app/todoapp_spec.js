f19ba76230110ea07833e8ab98cfd73fda8994a6
arnoutc <arnout@catorarn.com>
2019-07-01 16:18:47 +0100

    Changed cypress.io test to use data-cy attributes along best practices at https://docs.cypress.io/guides/references/best-practices.html

--- cypress/integration/todo-app/todoapp_spec.js
+++ cypress/integration/todo-app/todoapp_spec.js
@@ -6,19 +6,34 @@ describe('my first test', function(){
     cy.visit('http://localhost:8000');
 
     //set text
-    let el = cy.get('#new-todo');
+    let el = cy.get('[data-cy=new-todo]');
+    el.should('have.attr','placeholder','What needs to be done?');  
 
-    el.should('have.attr','placeholder','What needs to be done?');
+    let text = 'supercalifragilisticexpialidocious';
+    el.type(text);
 
-    el.type('supercalifragilisticexpialidocious');
-    el.should('have.value','supercalifragilisticexpialidocious');
-
-    // click on the priority button in the input box
-    let prio = cy.get('header button.priority-btn');
+    let prio = cy.get('[data-cy=priority-btn]');
     prio.trigger('click');
 
+    cy.get('[data-cy=new-todo]').should('have.value',text);
+
+  
     //then
-    cy.get('#todo-list li').should('have.class', 'priority');
+    cy.get('[data-cy=todo-list]').children().should('have.class', 'priority');
+
+
+    //when second item is added with priority
+
+    //there are 2 li's with priority class
+
+    //when third item is added without priority
+
+    //when click Priority filter
+
+    //then 2 items should show in the list
+
+
+
 
   });
 });
